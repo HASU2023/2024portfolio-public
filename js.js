f03a133f14103c18cbcb6a27c0361613c1662bf7
HASU2023 <hasu20232@gamil.com>
2024-02-05 14:18:21 +0900

    0205

--- js.js
+++ js.js
@@ -1,5 +1,25 @@
-/* ~~~~~~~~~~~~~~~~~~ menu toggle ~~~~~~~~~~~~~~~~~~ */
+
+// ~~~~~~~~~~~~~~~~~~ menu toggle ~~~~~~~~~~~~~~~~~~ 
 
 $(".click-menu").click(function () {
     $(".header-wide").toggleClass("menu-event");
 })
+
+// ~~~~~~~~~~~~~~~~~~ main : typing ani ~~~~~~~~~~~~~~~~~~ 
+
+var typingBool = false;
+var typingIdx = 0;
+var typingTxt = $(".typing-txt").text();
+typingTxt = typingTxt.split("");
+if (typingBool == false) {
+    typingBool = true;
+    var tyInt = setInterval(typing, 40);
+}
+function typing() {
+    if (typingIdx < typingTxt.length) {
+        $(".typing").append(typingTxt[typingIdx]);
+        typingIdx++;
+    } else {
+        clearInterval(tyInt);
+    }
+}
